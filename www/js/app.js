db12db06c4dad097856f5c91940284bbd3b10f3d
Endika <endikae94@gmail.com>
2015-09-07 16:22:48 +0200

    Data access implemented (cordovaSQLite) and AddPlatform finished

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('crypto', ['ionic', 'crypto.controllers'])
+angular.module('crypto', ['ionic', 'crypto.controllers', 'ngCordova'])
 
-  .run(function ($ionicPlatform) {
+  .run(function ($ionicPlatform, $rootScope, DBAdminService) {
     $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
@@ -15,6 +15,20 @@ angular.module('crypto', ['ionic', 'crypto.controllers'])
         if (window.StatusBar) {
           StatusBar.styleDefault();
         }
+
+        DBAdminService.initDB();
+
+        $rootScope.COLORS = [
+          {'color': 'light'},
+          {'color': 'stable'},
+          {'color': 'positive'},
+          {'color': 'calm'},
+          {'color': 'balanced'},
+          {'color': 'energized'},
+          {'color': 'assertive'},
+          {'color': 'royal'},
+          {'color': 'dark'}
+        ];
       }
     );
   })
